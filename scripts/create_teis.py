b4246849252103f6845def66765e1eb5b10da73e
csae8092 <peter.andorfer@oeaw.ac.at>
2022-12-22 13:45:12 +0100

    valid teis

--- scripts/create_teis.py
+++ scripts/create_teis.py
@@ -2,10 +2,12 @@ import os
 import pandas as pd
 import jinja2
 from collections import defaultdict
-
+from datetime import date
 from sqlalchemy import create_engine
 from tqdm import tqdm
 
+
+today = date.today()
 out_dir = './data/editions'
 os.makedirs(out_dir, exist_ok=True)
 templateLoader = jinja2.FileSystemLoader(searchpath="./scripts/templates")
@@ -16,7 +18,7 @@ DB_USER = os.environ.get("DB_USER")
 DB_PW = os.environ.get("DB_PW")
 DB_NAME = os.environ.get("DB_NAME")
 DB_HOST = os.environ.get("DB_HOST", "127.0.0.1")
-N = 20
+N = None
 
 db_connection_str = f"mysql://{DB_USER}:{DB_PW}@{DB_HOST}/{DB_NAME}"
 db_connection = create_engine(db_connection_str)
@@ -24,6 +26,10 @@ db_connection = create_engine(db_connection_str)
 query = "SELECT * FROM synopse"
 df = pd.read_sql(query, con=db_connection)
 df.to_csv("hansi.csv", index=False)
+if N:
+    pass
+else:
+    N = len(df)
 
 film_df = pd.read_sql("SELECT * FROM filminfo", con=db_connection)
 film_df.to_csv("film_df.csv", index=False)
@@ -56,6 +62,7 @@ for i, row in tqdm(df.head(N).iterrows(), total=N):
     context["kino_data"] = kino_data
     context["wirte_data"] = wirte_data
     context["filme"] = films
+    context["current_date"] = f'{today.strftime("%Y-%m-%d")}'
     file_name = os.path.join(out_dir, f"ask__{context['datum']}.xml")
     with open(file_name.lower(), 'w') as f:
-        f.write(template.render(**context))
+        f.write(template.render(**context).replace('&', '&amp;').replace('<u>', '<hi>').replace('</u>', '</hi>'))
